0a27b33f2d1172381e507b051325caed3b2909ae
Dasha <49146036+shpakovad@users.noreply.github.com>
2020-04-01 16:33:26 +0300

    Updates

---
+++ precache-manifest.04ea9d895284c1e7747139983772c460.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7d4bd96bf5c39f070a80c4d3a48df01",
+    "url": "/Simple-Tree-React-js/index.html"
+  },
+  {
+    "revision": "05873619ea3f64bd2ec3",
+    "url": "/Simple-Tree-React-js/static/css/main.f8d19bd4.chunk.css"
+  },
+  {
+    "revision": "ff9c97ca97578027f3dc",
+    "url": "/Simple-Tree-React-js/static/js/2.06abb2dc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Simple-Tree-React-js/static/js/2.06abb2dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05873619ea3f64bd2ec3",
+    "url": "/Simple-Tree-React-js/static/js/main.f9d67f60.chunk.js"
+  },
+  {
+    "revision": "5d97c6578619c0f92eaa",
+    "url": "/Simple-Tree-React-js/static/js/runtime-main.3a17d2ab.js"
+  }
+]);
